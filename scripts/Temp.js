50f0660e62e2d21f3e3892689a495b5a0946387b
werewolf lord <40964777+alwx2008@users.noreply.github.com>
2023-02-07 22:53:00 -0600

    Create Temp.js

---
+++ scripts/Temp.js
@@ -0,0 +1,19 @@
+let temp = document.getElementById('temp');
+let tempUnit = document.getElementsByTagName('tempUnit');
+
+function convertTemp() {
+  if (tempUnit.value == "F") {
+    let F = temp.value;
+    let C = ( 5 / 9 ) * ( temp.value - 32 );
+    let K = ( 5 / 9 ) * ( temp.value - 32 ) + 273.16;
+  } else if (tempUnit.value == "C") {
+    let F = ( 9 / 5 ) * temp.value + 32;
+    let C = temp.value;
+    let K = temp.value + 273.16;
+  } else if (tempUnit.value == "K") {
+    let F = (( 9 / 5 ) * (temp.value - 273.15)) + 32;
+    let C = temp.value - 273.16;
+    let K = temp.value;
+  }
+  return F, C, K;
+}
