502edd8390652677bf08c750b9fff0aef39a1676
werewolf lord <40964777+alwx2008@users.noreply.github.com>
2023-02-07 22:54:17 -0600

    Create Wind.js

---
+++ scripts/Wind.js
@@ -0,0 +1,27 @@
+let speed = document.getElementById('speed');
+let speedUnit = document.getElementsByTagName('speedUnit');
+
+function convertSpeed() {
+  if (speedUnit.value == "MPH") {
+    let MPH = speed.value;
+    let KTS = 0.8689762 * speed.value;
+    let FTS = 1.46667 * speed.value;
+    let KMH = 1.609344 * speed.value;
+  } else if (speedUnit.value == "KTS") {
+    let MPH = 1.1507794 * speed.value;
+    let KTS = speed.value;
+    let FTS = 1.6878099 * speed.value;
+    let KMH = 1.852 * speed.value;
+  } else if (speedUnit == "FT/S") {
+    let MPH = 0.681818 * speed.value;
+    let KTS = 0.5924838 * speed.value;
+    let FTS = speed.value;
+    let KMH = 1.09728 * speed.value;
+  } else if (speedUnit.value == "KM/H") {
+    let MPH = 0.621371 * speed.value;
+    let KTS = 0.5399568 * speed.value;
+    let FTS = 0.911344 * speed.value;
+    let KMH = speed.value;
+  }
+  return MPH, KTS, FTS, KMH;
+}
